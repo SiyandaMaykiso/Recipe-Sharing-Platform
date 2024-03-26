61652f029eb9d8781067c1fd8f284bcb31ce4b6e
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-26 11:55:56 -0500

    Adjust comment date to created at

--- migrations/20240326144054-create-comment.js
+++ migrations/20240326144054-create-comment.js
@@ -10,17 +10,28 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       recipeId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Recipes', // Name of the table as defined in the database
+          key: 'id',
+        },
+        onDelete: 'CASCADE' // Ensure this matches your model definition
       },
       userId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users', // Name of the table as defined in the database
+          key: 'id',
+        },
+        onDelete: 'CASCADE' // Ensure this matches your model definition
       },
       commentText: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false
       },
-      commentDate: {
-        type: Sequelize.DATE
-      },
+      // Removed the commentDate field
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/comment.js
+++ models/comment.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Comment extends Model {
@@ -47,12 +45,8 @@ module.exports = (sequelize, DataTypes) => {
       validate: {
         notEmpty: { msg: "Comment text must not be empty" },
       }
-    },
-    commentDate: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      // If using Sequelize's `createdAt`, this field may be redundant
     }
+    // Removed the commentDate field to rely on Sequelize's `createdAt`
   }, {
     sequelize,
     modelName: 'Comment',
