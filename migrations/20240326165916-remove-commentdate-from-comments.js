9c2584df44b927e9a72fdcd2f533ba471b4cf081
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-26 14:19:10 -0500

    Adjust migrations

--- migrations/20240326165916-remove-commentdate-from-comments.js
+++ migrations/20240326165916-remove-commentdate-from-comments.js
@@ -2,16 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // Remove the commentDate column
-    await queryInterface.removeColumn('Comments', 'commentDate');
+    const tableDescription = await queryInterface.describeTable('Comments');
+    if (tableDescription.commentDate) {
+      await queryInterface.removeColumn('Comments', 'commentDate');
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
     // Add the commentDate column back if the migration is rolled back
-    await queryInterface.addColumn('Comments', 'commentDate', {
-      type: Sequelize.DATE,
-      allowNull: false,
-      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-    });
+    const tableDescription = await queryInterface.describeTable('Comments');
+    if (!tableDescription.commentDate) {
+      await queryInterface.addColumn('Comments', 'commentDate', {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      });
+    }
   }
 };
