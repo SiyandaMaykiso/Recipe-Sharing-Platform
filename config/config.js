cff72cccb2812dcdd916c588ddb6a9ad07aa1102
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-26 14:11:14 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD, // No fallback here, to ensure it uses the env variable
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT
-  }
+  },
+  // other environments
 };
