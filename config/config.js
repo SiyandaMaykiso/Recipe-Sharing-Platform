71decf44ed567a534deb612308b29cdd052d59e8
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-26 14:04:20 -0500

    Adjust config file

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
 module.exports = {
   development: {
     username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD, // No fallback here, to ensure it uses the env variable
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
     port: process.env.DB_PORT
-  },
-  // Add configurations for test and production if needed
+  }
 };
