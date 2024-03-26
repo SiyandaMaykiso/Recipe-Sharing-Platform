1d4aac1ced55223bd4847d6cae8ddc6ff66e2e32
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-26 13:21:37 -0500

    Remove .env from version control

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT
+  }
+  // Add other environments (test, production) as needed
+};
