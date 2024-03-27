11e68c4c7dc12c0972ef80d9af20ca84569a3c80
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-27 13:42:24 -0500

    Adjust controllers

--- controllers/recipesController.js
+++ controllers/recipesController.js
@@ -6,7 +6,13 @@ exports.createRecipe = async (req, res) => {
 };
 
 exports.getAllRecipes = async (req, res) => {
-    // Logic to get all recipes
+    try {
+        const recipes = await Recipe.findAll();
+        res.json(recipes);
+    } catch (error) {
+        console.error("Error fetching recipes: ", error);
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
 };
 
 exports.getRecipeById = async (req, res) => {
