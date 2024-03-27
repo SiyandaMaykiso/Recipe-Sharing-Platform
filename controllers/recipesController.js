868d1a1651e574358b19f8f4b276bffa75973daa
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-03-27 10:23:27 -0500

    Create recipesController.js

---
+++ controllers/recipesController.js
@@ -0,0 +1,22 @@
+// controllers/recipesController.js
+const { Recipe } = require('../models');
+
+exports.createRecipe = async (req, res) => {
+    // Logic to create a recipe
+};
+
+exports.getAllRecipes = async (req, res) => {
+    // Logic to get all recipes
+};
+
+exports.getRecipeById = async (req, res) => {
+    // Logic to get a single recipe by id
+};
+
+exports.updateRecipe = async (req, res) => {
+    // Logic to update a recipe by id
+};
+
+exports.deleteRecipe = async (req, res) => {
+    // Logic to delete a recipe by id
+};
