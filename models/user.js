4435b47f4dee44dfd39d54c7c08267411004a289
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-18 13:45:29 -0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const db = require('../db');
+const bcrypt = require('bcryptjs');  // Make sure bcryptjs is imported
 
 const User = {
   async create(username, email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);  // Hash the password with bcrypt
     const query = 'INSERT INTO Users(username, email, password) VALUES($1, $2, $3) RETURNING *';
-    const values = [username, email, password];
+    const values = [username, email, hashedPassword];  // Use hashedPassword instead of password
     try {
       const { rows } = await db.query(query, values);
       return rows[0];
@@ -38,31 +40,30 @@ const User = {
   },
 
   async update(userId, updateFields) {
-    
     const fieldsToUpdate = Object.entries(updateFields).reduce((acc, [key, value]) => {
       if (value !== null && value !== undefined) {
         acc[key] = value;
       }
       return acc;
     }, {});
-  
+
     if (Object.keys(fieldsToUpdate).length === 0) {
       throw new Error("No valid fields provided for update");
     }
-  
+
     let query = 'UPDATE Users SET ';
     const queryParams = [];
     const queryParts = [];
-  
+
     Object.keys(fieldsToUpdate).forEach((field, index) => {
       const columnName = field === 'profileImagePath' ? 'profile_image_path' : field;
       queryParts.push(`${columnName} = $${index + 1}`);
       queryParams.push(fieldsToUpdate[field]);
     });
-  
+
     query += queryParts.join(', ') + ' WHERE user_id = $' + (queryParams.length + 1) + ' RETURNING *';
     queryParams.push(userId);
-  
+
     try {
       const { rows } = await db.query(query, queryParams);
       return rows.length > 0 ? rows[0] : null;
