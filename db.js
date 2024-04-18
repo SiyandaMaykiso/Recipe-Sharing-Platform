4a5f0e86976787a5ff094224bc2650164ec53106
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-18 12:25:47 -0500

    Configure SSL for PostgreSQL on Heroku

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 const { Pool } = require('pg');
 
+// Determine SSL usage based on environment
+const useSSL = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Required to trust Heroku's self-signed certificate
-  }
+  ssl: useSSL ? {
+    rejectUnauthorized: false // This is important to avoid "self-signed certificate" issues
+  } : false
 });
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
