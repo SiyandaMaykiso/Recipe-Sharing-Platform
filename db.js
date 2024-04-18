6a00f84abb3bb63fefec2249e2eaa58acc312733
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-18 12:15:16 -0500

    Adjust database config

--- db.js
+++ db.js
@@ -1,13 +1,8 @@
-require('dotenv').config();
 const { Pool } = require('pg');
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Required to trust Heroku's self-signed certificate
+  }
+});
