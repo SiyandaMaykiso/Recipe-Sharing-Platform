aaecbcefeb26cd67804202af79dd98029bea7eba
Siyanda Mayekiso <s.mayekiso@gmail.com>
2024-04-22 13:07:04 -0500

    Adjust app.css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { AuthProvider, useAuth } from './contexts/AuthContext'; // Ensure useAuth is imported
 import UserProfile from './components/UserProfile';
 import RecipeForm from './components/RecipeForm';
 import Home from './components/Home';
@@ -14,52 +14,22 @@ import RecipeListings from './components/RecipeListings';
 console.log("Starting the application...");
 
 // Helper component for private routes
-const PrivateRoute = ({ children }) => {
-  const { authToken } = useAuth();
+function PrivateRoute({ children }) {
+  const { authToken } = useAuth(); // Correct usage of useAuth inside a component
   return authToken ? children : <Navigate to="/login" />;
-};
+}
 
 function App() {
-  const { authToken } = useAuth(); // Assuming useAuth is exported correctly and can be used here
-
-  const recipeFormInitialValues = {
-    title: '',
-    description: '',
-    ingredients: [{ name: '', quantity: '' }],
-  };
-
-  const handleRecipeFormSubmit = async (values, setFormError) => {
-    try {
-      const response = await fetch('https://recipe-sharing-platform-sm-8996552549c5.herokuapp.com/recipes', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${authToken || ''}`, // Use authToken from context
-        },
-        body: JSON.stringify(values),
-      });
-
-      if (!response.ok) {
-        const errorData = await response.json();
-        throw new Error(errorData.message || 'Failed to create recipe');
-      }
-
-      const recipe = await response.json();
-      console.log('Recipe created successfully', recipe);
-    } catch (error) {
-      console.error('Failed to create recipe:', error);
-      if (setFormError) {
-        setFormError(error.message || 'An unexpected error occurred. Please try again.');
-      }
-    }
-  };
-
   return (
     <Router>
       <AuthProvider>
         <Routes>
           <Route path="/user" element={<PrivateRoute><UserProfile /></PrivateRoute>} />
-          <Route path="/add-recipe" element={<PrivateRoute><RecipeForm initialValues={recipeFormInitialValues} onSubmit={handleRecipeFormSubmit} /></PrivateRoute>} />
+          <Route path="/add-recipe" element={
+            <PrivateRoute>
+              <RecipeForm />
+            </PrivateRoute>
+          } />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
           <Route path="/" element={<Home />} />
